664c8a43e3021ad8497e7e85955e7ba94a3db04f
khois345 <khois345@gmail.com>
2024-10-01 17:17:34 -0700

    Added summary page

--- financeTracker/src/App.jsx
+++ financeTracker/src/App.jsx
@@ -4,6 +4,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login';
 import Signup from './pages/Signup'
+import Summary from './pages/Summary';
 function App() {
   return (
     <div>
@@ -13,6 +14,7 @@ function App() {
           <Route path = '/home' element = {<Home/>}/>
           <Route path = '/login' element = {<Login/>}/>
           <Route path = '/signup' element = {<Signup/>}/>
+          <Route path ='/summary' element = {<Summary/>}/>
         </Routes>
       </BrowserRouter>
     </div>
