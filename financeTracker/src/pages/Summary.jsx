d013a2edaf54e45ec7d2d52e25f1fa094b201522
khois345 <khois345@gmail.com>
2024-10-02 20:48:20 -0700

    Updating more similar styling + add filter icon buttons

--- financeTracker/src/pages/Summary.jsx
+++ financeTracker/src/pages/Summary.jsx
@@ -3,15 +3,18 @@ import React from "react";
 export default function Summary() {
   return (
     // W-[100vw] is to cover the whole screen with white background, overlapping the black bg in app.css
-    <div className="h-screen flex flex-row justify-start w-[100vw] bg-neutral-50 relative">
-
+    <div className="h-screen flex flex-row justify-start w-[100vw] bg-neutral-200 relative">
       {/* Sidebar */}
-      <div className="flex flex-col w-48 fixed left-0 top-0 h-full items-center bg-black">
-        <img
-          src="./logoIcon.png"
-          alt="Company Logo"
-          className="h-[75px] w-[75px] object-center"
-        />
+      <div className="flex flex-col w-44 fixed left-0 top-0 h-full items-center bg-black">
+        {/* Icon */}
+        <div className="pt-3">
+          <img
+            src="./logoIcon.png"
+            alt="Company Logo"
+            className="h-[70px] w-[70px] object-center"
+          />
+        </div>
+        {/* Navigation */}
         <nav className="pt-10">
           <ul className="flex flex-col">
             <li className="flex gap-2 items-center">
@@ -52,32 +55,64 @@ export default function Summary() {
 
       {/* Summary content */}
       <div className="flex-1 ml-48 w-full h-full flex text-black overflow-y-scroll">
+        {/* Summary Card Container */}
         <div className="pt-10 flex flex-wrap w-full h-full gap-10 justify-evenly">
-          <div className="w-1/3 h-1/5 bg-white shadow-md rounded-lg p-4">
-            <h1 className="text-lg md:text-2xl lg:text-3xl">
-              This month's expenses:
-            </h1>
-            <h2 className="text-3xl font-bold md:text-xl lg:text-2xl p-2">
-              $1234.56
-            </h2>
+          {/* Expense Card */}
+          <div className="w-1/3 h-1/5 bg-white p-4">
+            <div className="items-center flex justify-between">
+              <h1 className="text-lg">This month's expenses:</h1>
+              <img
+                src="./summaryPage/editButton.png"
+                alt="Edit Button"
+                className="h-10 w-10"
+              />
+            </div>
+            <h2 className="text-3xl lg:text-4xl font-bold p-2">$1234.56</h2>
           </div>
-          <div className="w-1/3 h-1/5 bg-white shadow-md rounded-lg p-4">
-            <h1 className="text-lg md:text-2xl lg:text-3xl">Savings:</h1>
-            <h2 className="text-3xl font-bold md:text-xl lg:text-2xl p-2">
-              $1234.56
-            </h2>
+
+          {/* Savings Card */}
+          <div className="w-1/3 h-1/5 bg-white p-4">
+            <div className="items-center flex justify-between">
+              <h1 className="text-lg">Savings:</h1>
+              <img
+                src="./summaryPage/editButton.png"
+                alt="Edit Button"
+                className="h-10 w-10"
+              />
+            </div>
+            <h2 className="text-3xl lg:text-4xl font-bold p-2">$1234.56</h2>
           </div>
-          <div className="w-[80%] h-[40vh] bg-white shadow-md rounded-lg p-4">
-            <h1 className="text-lg md:text-2xl lg:text-3xl">
-              Earnings/Expenses Monthly Comparison
-            </h1>
-            <div className="w-full h-full">...Insert graph here</div>
+
+          {/* Earnings/Expense Monthly Comparison Card */}
+          <div className="w-[80%] h-[40vh] bg-white p-4">
+            <h1 className="text-lg">Earnings/Expenses Monthly Comparison</h1>
+            <div className="w-full h-full pt-2">...Insert graph here</div>
           </div>
-          <div className="w-[80%] h-[40vh] bg-white shadow-md rounded-lg p-4">
-            <h1 className="text-lg md:text-2xl lg:text-3xl">
-              Expenses Breakdown:
-            </h1>
-            <div className="w-full h-full">...Insert table here</div>
+
+          {/* Expenses Breakdown Card */}
+          <div className="w-[80%] h-[40vh] bg-white p-4">
+            <div className="items-center flex justify-between">
+              <h1 className="text-lg font-bold">Expenses Breakdown:</h1>
+              <div className="flex gap-2" id="filter-icons">
+                {/* Has yet to add icon tags due to CSS file conflicting */}
+                <img
+                  src="./summaryPage/filterIcon.png"
+                  alt="Filter Icon"
+                  className="h-5 w-5"
+                />
+                <img
+                  src="./summaryPage/encloseIcon.png"
+                  alt="Enclose Icon"
+                  className="h-5 w-5"
+                />
+                <img
+                  src="./summaryPage/openIcon.png"
+                  alt="Open Button"
+                  className="h-5 w-5"
+                />
+              </div>
+            </div>
+            <div className="w-full h-full pt-2">...Insert table here</div>
           </div>
         </div>
       </div>
