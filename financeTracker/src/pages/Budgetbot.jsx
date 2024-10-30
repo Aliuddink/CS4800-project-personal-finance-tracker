b7d97a1e23223028a16a90edade39e32e75258fb
Matthew YU <myu@cpp.edu>
2024-10-29 23:55:57 -0700

    Gemini Bot

--- financeTracker/src/pages/Budgetbot.jsx
+++ financeTracker/src/pages/Budgetbot.jsx
@@ -1,23 +1,18 @@
-import ollama from 'ollama'
+import { GoogleGenerativeAI } from "@google/generative-ai";
 import React, { useState } from 'react';
 
 const InputBox = () => {
   const [inputValue, setInputValue] = useState('');
   const [submittedValue, setSubmittedValue] = useState('');
-
+  const genAI = new GoogleGenerativeAI('AIzaSyDzSTXab-1xDNFF56CxNjv8WxcPb8hgLzU');
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
 
   const handleSubmit = async () => {
-    const response = await ollama.chat({
-        model: 'mistral',
-        messages: [{ role: 'user', content: inputValue }],
-        temperature: 0.9, // Adjust for creativity
-        top_p: 0.9, // Adjust for diversity
-    })
-    console.log(response)
-    setSubmittedValue(response.message.content);
+    const result = await model.generateContent(inputValue);
+    setSubmittedValue(result.response.text());
     setInputValue(''); // Optionally clear the input after submission
   };
 
@@ -28,7 +23,7 @@ const InputBox = () => {
         value={inputValue}
         onChange={handleInputChange}
         placeholder="Type something..."
-        style={{ padding: '10px', width: '100%', marginBottom: '10px' }}
+        style={{ padding: '10px', width: '100%', marginBottom: '10px', color: 'black' }}
       />
       <button onClick={handleSubmit} style={{ padding: '10px', width: '100%' }}>
         Submit
