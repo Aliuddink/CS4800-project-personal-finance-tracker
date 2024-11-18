6f04b38ba3078af3769e4a03fa452707bc9338a0
Matthew YU <myu@cpp.edu>
2024-11-18 00:03:16 -0800

    Support tyopes

--- financeTracker/src/components/Scanner.jsx
+++ financeTracker/src/components/Scanner.jsx
@@ -6,7 +6,19 @@ const qrcodeRegionId = "html5qr-code-full-region";
 
 // Creates the configuration object for Html5QrcodeScanner.
 const createConfig = (props) => {
-    let config = {};
+    let config = {
+        formatsToSupport: [
+            Html5QrcodeSupportedFormats.QR_CODE, // QR Code
+            Html5QrcodeSupportedFormats.CODE_128, // Barcode
+            Html5QrcodeSupportedFormats.CODE_39,
+            Html5QrcodeSupportedFormats.EAN_13,
+            Html5QrcodeSupportedFormats.CODE_93,
+            Html5QrcodeSupportedFormats.ITF,
+            Html5QrcodeSupportedFormats.EAN_8,
+            Html5QrcodeSupportedFormats.UPC_E,
+            Html5QrcodeSupportedFormats.UPC_A,
+        ]
+    };
     if (props.fps) {
         config.fps = props.fps;
     }
